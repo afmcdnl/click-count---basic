1674228a28c8477b9ff5d3f2d556acb061d412be
Andrew <afmcdnl@gmail.com>
2020-07-29 21:31:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    x += 1
+    basic.showNumber(x)
+})
+input.onButtonPressed(Button.B, function () {
+    x += -1
+    basic.showNumber(x)
+})
+let x = 0
+x = 0
+basic.showNumber(x)
